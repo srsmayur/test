d94c02c4074ec6c4bcd0dd87454e3ff30e269da5
Srs Mayur <srs.mayur@gmail.com>
2018-02-27 12:39:54 +0530

    extend confusion

--- Custom-Module-JavaScript/modul.js
+++ Custom-Module-JavaScript/modul.js
@@ -12,7 +12,29 @@ var modularpattern = (function(){
         }
     }
 }());
-alert(modularpattern.add());
-alert(modularpattern.add());
-alert(modularpattern.reset());
+
+var modularpattern1 = (function(){
+
+    var sum = 0;
+
+    return {
+        add:function(){
+            sum = sum + 1;
+            return sum;
+        },
+        reset:function(){
+            return sum = 0;
+        }
+    }
+}());
+
+var modularpattern2 = (function(){
+
     
+})
+    
+var opts = $.extend(modularpattern, modularpattern1)
+
+alert(opts.add());
+alert(opts.add());
+alert(opts.reset());
